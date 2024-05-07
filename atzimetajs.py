1f83e3c52d9080cd4dba4b96832615f0e5e09ae1
Atis Dubrovskis <53487681+atisDu@users.noreply.github.com>
2024-05-07 11:24:25 +0300

    Add files via upload

--- atzimetajs.py
+++ atzimetajs.py
@@ -1,4 +1,3 @@
-import numpy as np
 import json
 
 prieksmeti = ["Matemātika", "Latviešu valoda", "Sports", "Programmēšana", "Mongoļu grozu pīšanas māksla"]
@@ -6,40 +5,54 @@ atzimes = [[1, 2, 3, 4, 5],[4, 5, 8, 9],[3, 4, 6, 7],[6,3,2,9,10],[10,10,10,10,1
 
 kopums = {}
 for prieksas in prieksmeti:
-    for atskaites in atzimes:
+    for atskaites in atzimes: #pievieno visu sarakstu datus vārdnīcai
         kopums[prieksas] = atskaites
         atzimes.remove(atskaites)
         break
 
-
-
-def Videjais(lst): 
-    return sum(lst) / len(lst) 
+def videjais(lst): 
+    return sum(lst) / len(lst)  #aprēķina vidējo vērtību katram atzīmju kopumam
 
 def rakstit():
     global vards
-    global prieksmeti 
+    global prieksmeti  #norāda globālo mainīto izmantošanu
     global atzimes 
     with open(f'atzimes_{vards}.json', 'a', encoding='utf-8') as f: #atver json failu ar lietotāja ievadīto vārdu
-        for index in range(len(prieksmeti)):
-            json.dump(str(prieksmeti[index]) + "/n" + str(atzimes[index]) + "\n", f) #json.dump lai rakstitu datus json faila
-        # vajag izdomat, ka to rakstit un lasit ta, lai butu sasaistiti divi list: prieksmeti un atzimes
-
+        prieksmetu_skaits = int(input("Ievadiet priekšmetu skaitu: ")) 
+        for i in range (0, prieksmetu_skaits): 
+            prieksmets = input("Ievadi %r. priekšmeta nosaukumu: " % (i+1)) #ļauj ievadīt priekšmeta nosaukumu tik reizes, cik lietotājs ievadījis priekšmetu skaitu
+            atzime = input("Ievadi %r. prieksmeta atzīmes, atdalot tās ar komatu (1, 2, 3): " % (i+1)).split(', ') #atdala atzīmes, katru kā int
+            #Ievada kopuma vardnīcā
+            kopums[prieksmets] = atzime
+        #Ievada vārdnīcu json failā, pārveidojot to par JS objektu
+        json.dump(kopums, f)
+ 
 def parrakstit():
     global vards  #atver failu ar "w", izdzēšot visus bijušos datus
     with open(f'atzimes_{vards}.json', 'w', encoding='utf-8') as f:
-        json.dump(f)
+        kopums = {}
+        prieksmetu_skaits = int(input("Ievadiet priekšmetu skaitu: ")) 
+        for i in range (0, prieksmetu_skaits): 
+            prieksmets = input("Ievadi %r. priekšmeta nosaukumu: " % (i+1)) #ļauj ievadīt priekšmeta nosaukumu tik reizes, cik lietotājs ievadījis priekšmetu skaitu
+            atzime = input("Ievadi %r. prieksmeta atzīmes, atdalot tās ar komatu (1, 2, 3): " % (i+1)).split(', ') #atdala atzīmes, katru kā int
+            #Ievada kopuma vardnīcā
+            kopums[prieksmets] = atzime
+        json.dump(kopums, f)
 
+def ielasit():
+    global vards
+    with open(f'atzimes_{vards}.json', 'r', encoding='utf-8') as f:
+        kopums = json.load(f)
+        
+        
 #3.1 (Atis) Pabeigts
 def ka_iegut_videjo():
-    
     print("----------\nAtzīmes:\n")
-    for i in range(len(prieksmeti)):
-        #videja = Videjais(atzimes[i])
-        print("%r)%r (vid. %r)" % (i,prieksmeti[i], Videjais(atzimes[i])))
+    for i in range(int(len(prieksmeti))):
+        #videja = videjais(atzimes[i])
+        print("%r)%r (vid. %r)" % (i,prieksmeti[i], videjais(atzimes[i])))
     print("----------")
     prieks_izvele = int(input("Kurā priekšmetā vēlies labot vidējo? (nr. pēc kārtas): "))
-    min_cipars = min(atzimes[i])
     min_pec_kartas = atzimes[i].index(min(atzimes[i])) 
     
     print("Tu vari izlabot vidējo atzīmi priekšmetā: %r,\nja tu izlabo %r. atzīmi uz:" % (prieksmeti[prieks_izvele], min_pec_kartas + 1))
@@ -47,14 +60,14 @@ def ka_iegut_videjo():
     print("vid. (mainīsies uz):")
     for i in range (1, 10):
         atzimes[min_pec_kartas] = i
-        print("%r" % Videjais(atzimes[prieks_izvele]),sep="|")
-    izvelne()
+        print("%r" % videjais(atzimes[prieks_izvele]),sep="|")
+    galvenais()
     
 #3.2 (atis) Pabeigts
 def ka_ietekme_videjo():
     print("Priekšmeti:\n")
     for i in range(len(prieksmeti)):
-        #videja = Videjais(atzimes[i])
+        #videja = videjais(atzimes[i])
         print("%r)%r" % (i,prieksmeti[i]))
     prieks_izvele = int(input("Ievadi priekšmeta nr. (pēc kartas), kura atzīmi vēlies analizēt: "))
     print("----------")
@@ -63,50 +76,60 @@ def ka_ietekme_videjo():
     print("Ja %r. atzīmi labotu uz:\n1|2|3|4|5|6|7|8|9|10,\ntad vidējā atzīmi būtu:")
     for i in range (1, 10):
         atzimes[kuru_atzimi] = i
-        print("%r" % Videjais(atzimes[prieks_izvele]),sep="|")
+        print("%r" % videjais(atzimes[prieks_izvele]),sep="|")
 
-ka_iegut_videjo()
-
-''''
-        
-
-mahinacija()
-'''
-'''
 def prognoze():
     try:
-        print(f"Priekšmeti: {prieksmeti}")
-        prieksmeta_numurs = int(input("Ievadi priekšmeta nr. (pēc kārtas), kura atzīmi vēlies analizēt:"))
-        if prieksmeta_numurs == 1:
-            print(f"Atzīmes atlasītajā priekšmetā: {atzimes[0]}")
-            jauna_atzime 
-        elif prieksmeta_numurs == 2:
-            print(f"Atzīmes atlasītajā priekšmetā: {atzimes[1]}")
-        elif prieksmeta_numurs == 3:
-            print(f"Atzīmes atlasītajā priekšmetā: {atzimes[2]}")
-        elif prieksmeta_numurs == 4:
-            print(f"Atzīmes atlasītajā priekšmetā: {atzimes[3]}")
-    except ValueError:
-        print('Nepareiza datu ievade')
+        print(f"Priekšmeti:\n1){prieksmeti[0]}\n2){prieksmeti[1]}\n3){prieksmeti[2]} \n3){prieksmeti[2]} \n4){prieksmeti[3]}")
+        prieksmeta_numurs = int(input("Ievadi priekšmeta nr. (pēc kārtas), kura atzīmi vēlies analizēt:")) 
+        if prieksmeta_numurs == 1: # ja lietotājs ievada 1 tad 
+            print(f"Atzīmes {prieksmeti[0]} : {atzimes[0]}")
+            print('Ja iegūst \n1|2|3|4|5|6|7|8|9|10\n tad vidējā atzīmi būtu: ')
             
-'''    
+
+            print('Kopējā vidējā atzīme:')
+            
+        elif prieksmeta_numurs == 2:
+            print(f"Atzīmes {prieksmeti[1]}: {atzimes[1]}")  
+        elif prieksmeta_numurs == 3:
+            print(f"Atzīmes {prieksmeti[2]}: {atzimes[2]}")
+        elif prieksmeta_numurs == 4:
+            print(f"Atzīmes {prieksmeti[3]}: {atzimes[3]}")
+    except ValueError:
+        print('Nepareiza datu ievade') 
 
 
 def galvenais():
-    print("\n**********************************")
-    print ("Sveicināti esat, filozofi un mācoņi, šai programmā lieliskajā. Šajā aparātā jūs varat ievadīt savus zināšanu skaitļu mērus un analizēt tos.")
-    print("\n**********************************")
-    izvele = ("Vai vēlies ievadīt datus no jauna, vai ielasīt no faila? (jauns/ielasit): (str tips)")
- 
-    izvelne = ("Izvēlne 1 : Par cik jāuzlabo zemākā atzīme, lai iegūtu vēlamo vidējo vērtējumu \n 2 : Kā noteiktā atzīme ietekmē vidējo atzīmi priekšmetā un kopumā. \n 3 : Ievadīt jaunus datus, papildinot iepriekšējos. \n4 : Pārrakstīt visus datus.\n“iziet” : Beigt programmas darbību (“iziet” var ievadīt jebkurā brīdī)")
-    opcijas = int(input("Izvēlies kādu no dotajām opcijām:"))
-    if opcijas == '1':
-        ka_iegut_videjo()
-    elif opcijas == '2':
-        prognoze()
-    elif opcijas == '3':
-            
-
-    
-def izvelne():
-    print("")
+        print("**********************************") #sveiciens
+        print ("Sveicināti esat, filozofi un mācoņi, šai programmā lieliskajā. \nŠajā aparātā jūs varat ievadīt savus zināšanu skaitļu mērus un analizēt tos.")
+        print("**********************************")
+        global vards
+        izvele = input("Vai vēlies ievadīt datus no jauna, vai ielasīt no faila? (jauns/ielasit):")
+        if izvele == "jauns":
+            vards = input("Ievadiet savu vārdu:")
+            rakstit() # izsauc funkciju  
+        elif izvele == "ielasit": 
+            vards = input("Ievadiet savu vārdu:")
+            ielasit()
+        print("1 : Par cik jāuzlabo zemākā atzīme, lai iegūtu vēlamo vidējo vērtējumu? \n2 : Kā noteiktā atzīme ietekmē vidējo atzīmi priekšmetā un kopumā? \n3 : Ievadīt jaunus datus, papildinot iepriekšējos. \n4 : Pārrakstīt visus datus, izdzēšot vecos.\n“iziet” : Beigt programmas darbību (“iziet” var ievadīt visas programmas laikā)\n—--------")
+        opcijas = int(input("Kuru darbību vēlaties veikt?:"))
+        if opcijas == '1':
+            ka_iegut_videjo()
+        elif opcijas == '2':
+            print("")
+            #prognoze()
+        elif opcijas == '3':
+            print()
+        elif opcijas == '4':
+            print("----------\nAtzīmes:\n")
+            for i in range(int(len(prieksmeti))):
+                print("%r)%r (vid. %r)" % (i,prieksmeti[i], videjais(atzimes[i])))
+            vai = input("Vai vēlaties pārrakstīt visus datus? (ja/ne):  ")
+            if vai == "ja":
+                print("Notiek datu dzēšana. . .")
+                print("Dati dzēsti.")
+                vards = input("Ievadiet savu vārdu:")
+                parrakstit()
+        elif opcijas == "iziet":
+            exit() #beidz programmu
+galvenais()
